c0d08bf2cf9da3b38df7dfa6d8c80d1e97d6f2c9
UyênCuong <nguyenthiuyencuong@gmail.com>
2020-09-23 21:48:00 +0700

    kiemthu

---
+++ test/Test Suites/Test Suite_login.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite_login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>01174d5d-c6b9-4984-a70e-86e09d299e33</testSuiteGuid>
+   <testCaseLink>
+      <guid>a1d175c2-a611-432c-b3ee-c758fc6d1223</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c2b0ecf8-310a-44cb-8d6b-d70c77aa2dc5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dab873fd-f731-4748-b1a9-5179b5ade6b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>48a03d35-e21c-4b5a-93b3-f82a58ef5564</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d30a68e0-2004-42c4-824f-7dcdd5698b79</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 05</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
